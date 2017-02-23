46a933adfd9bd69e7269e4ae7f929909e84c165a
vdab-pierre @desktop <pierre.colin@vdab.be>
2017-02-23 17:37:49 +0100

    http toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import{FormsModule} from '@angular/forms';
+import{HttpModule} from '@angular/http'
 
-
+//imports for loading & configuring the in memory web-api
+import{InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import{InMemoryDataService} from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -13,7 +16,8 @@ import { DashboardComponent } from './dashboard.component';
 import{AppRoutingModule} from './app-routing.module'
 
 @NgModule({
-  imports:      [ BrowserModule,FormsModule,AppRoutingModule],
+  imports:      [ BrowserModule,FormsModule,AppRoutingModule,HttpModule,
+                    InMemoryWebApiModule.forRoot(InMemoryDataService)],
   declarations: [ AppComponent,HeroDetailComponent,HeroesComponent,DashboardComponent ],
   bootstrap:    [ AppComponent ],
   providers:[HeroService]
